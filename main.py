5972260bc8a76b9da458fe3a0a874c21abffc823
AXDOOMER <alexandrexavier@live.ca>
2017-01-09 22:24:01 -0500

    Trouvé comment faire une deuxième page

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
+principale = ("<body><h1>Main</h1>" +
+    "<b>hello</b>" +
+    "</body>")
 
 #def hello_world(request):
 #    return Response('<body><h1>Hello World! %(name)s!</h1></body>' % request.matchdict)
 
+def vision(request):
+    return Response('<body><h1>Visionnement du status</h1></body>' % request.matchdict)
+
 def index(request):
-    return Response('<body><h1>Welcome mothafuckeurs!</h1></body>' % request.matchdict)
+    return Response(principale % request.matchdict)
 
 if __name__ == '__main__':
     config = Configurator()
-    #config.add_route('hello', '/hello/{name}')
-    #config.add_view(hello_world, route_name='hello')
+    config.add_route('vision', '/vision')
+    config.add_view(vision, route_name='vision')
 
     config.add_route('index', '/')
     config.add_view(index, route_name='index')
@@ -20,4 +26,4 @@ if __name__ == '__main__':
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
-   
+
