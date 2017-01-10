91bac26203fec28c77fa735ca146df4b96bc2053
AXDOOMER <alexandrexavier@live.ca>
2017-01-09 22:32:20 -0500

    Menus

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-principale = ("<body><h1>Main</h1>" +
-    "<b>hello</b>" +
+menu = "Menu: <a href=\"/\">Index</a> | <a href=\"/vision/\">Visionnement</a>"
+
+principale = ("<body><h1>Accueil</h1>" +
+    "<h3>Bienvenu sur votre serveur Jardiniot!</h3>" +
+    menu +
+    "</body>")
+
+visionnement = ("<body><h1>Visionnement du status</h1>" +
+    "<h3>Cette page vous permet de visionner le status de votre jardin.</h3>" +
+    menu +
     "</body>")
 
 #def hello_world(request):
 #    return Response('<body><h1>Hello World! %(name)s!</h1></body>' % request.matchdict)
 
 def vision(request):
-    return Response('<body><h1>Visionnement du status</h1></body>' % request.matchdict)
+    return Response(visionnement % request.matchdict)
 
 def index(request):
     return Response(principale % request.matchdict)
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('vision', '/vision')
+    config.add_route('vision', '/vision/')
     config.add_view(vision, route_name='vision')
 
     config.add_route('index', '/')
