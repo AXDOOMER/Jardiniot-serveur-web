9f1ab3cd1ba85c0610825f32735e0f2898cfce4d
AXDOOMER <alexandrexavier@live.ca>
2017-05-31 23:33:30 -0400

    Le fichier style.css est chargé de façon appropriée

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import markdown
 
-style = "<head><link rel=\"stylesheet\" href=\"styles.css\"></head>"
+style = "<head><link rel=\"stylesheet\" href=\"/static/styles.css\"></head>"
 
 menu = "<hr/>Menu: <a href=\"/\">Index</a> | <a href=\"/vision/\">Visionnement</a><hr/>"
 
-principale = (style + "<body><h1>Accueil</h1>" +
+principale = ("<body><h1>Accueil</h1>" +
     "<h3>Bienvenu sur votre serveur Jardiniot!</h3>" +
     menu +
     "La communication est établie avec vos jardins. Il y a <b>3</b> jardins détectés. " +
     "</body>")
 
-visionnement = (style + "<body><h1>Visionnement du status</h1>" +
+visionnement = ("<body><h1>Visionnement du status</h1>" +
     "<h3>Cette page vous permet de visionner le status de vos jardins.</h3>" +
     menu +
     "<table border=\"2\">" +
@@ -26,7 +26,7 @@ visionnement = (style + "<body><h1>Visionnement du status</h1>" +
     "<a href=\"/jardin/1\">1</a> | <a href=\"/jardin/2\">2</a> | <a href=\"/jardin/3\">3</a>" +
     "</body>")
 
-status = (style + "<body><h1>Visionnement du status du jardin %(name)s</h1>" +
+status = ("<body><h1>Visionnement du status du jardin %(name)s</h1>" +
     menu +
     "<table border=\"2\">" +
     "<tr><th>Temperature</th><th>22 °C</th></tr>" +
@@ -40,23 +40,20 @@ mdfile = open('README.md', 'r')
 md = mdfile.read()
 mdtest = markdown.markdown(md)
 
-cssfile = open('styles.css', 'r')
-css = cssfile.read()
+#cssfile = open('static/styles.css', 'r')
+#css = cssfile.read()
 
 def vision(request):
-    return Response(visionnement % request.matchdict)
+    return Response(style + visionnement % request.matchdict)
 
 def jardin(request):
-    return Response(status % request.matchdict)
+    return Response(style + status % request.matchdict)
 
 def index(request):
-    return Response(principale % request.matchdict)
+    return Response(style + principale % request.matchdict)
 
 def test(request):
     return Response(style + mdtest % request.matchdict)
-
-def styles(request):
-    return Response(css)
 
 if __name__ == '__main__':
     config = Configurator()
@@ -72,19 +69,7 @@ if __name__ == '__main__':
     config.add_route('test', '/test')
     config.add_view(test, route_name='test')
 
-    # stupid hack for styles.css, must be specified for every route.
-    config.add_route('styles.css', 'styles.css')
-    config.add_view(styles, route_name='styles.css')
-
-    config.add_route('md_styles.css', '/test/styles.css')
-    config.add_view(styles, route_name='md_styles.css')
-
-    config.add_route('vision_styles.css', '/vision/styles.css')
-    config.add_view(styles, route_name='vision_styles.css')
-
-    config.add_route('jardin_styles.css', '/jardin/styles.css')
-    config.add_view(styles, route_name='jardin_styles.css')
-    # delete and replace every line above until the comment.
+    config.add_static_view('static', '.', cache_max_age=3600)
 
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
